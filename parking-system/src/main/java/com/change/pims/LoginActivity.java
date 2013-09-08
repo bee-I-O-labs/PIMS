c59320db77d1c5d26c908a1f56e382053b0596a6
Raymond <rtukpe@gmail.com>
2013-09-08 20:43:38 +0100

    created the layout and activity for the login page

--- parking-system/src/main/java/com/change/pims/LoginActivity.java
+++ parking-system/src/main/java/com/change/pims/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.change.pims;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Creating the login activity.
@@ -9,5 +13,29 @@ import android.os.Bundle;
 public class LoginActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText uName = (EditText)findViewById(R.id.txtUserName);
+        EditText pWord = (EditText)findViewById(R.id.txtPassword);
+        Button btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        Button btnReg = (Button)findViewById(R.id.btnReg);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), SecurityActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
