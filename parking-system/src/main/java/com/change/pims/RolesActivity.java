97621351904aff82a4fb41ce23356bfaa7539bef
Lolu <lowluish@gmail.com>
2013-09-09 12:35:55 +0100

    Created gridview with 2 icons to navigate to admin/security.

--- parking-system/src/main/java/com/change/pims/RolesActivity.java
+++ parking-system/src/main/java/com/change/pims/RolesActivity.java
@@ -2,6 +2,10 @@ package com.change.pims;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 /**
  *should contain the gridview that consist of both the security and the admin
@@ -9,5 +13,25 @@ import android.os.Bundle;
 public class RolesActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_roles);
+        GridView gridview = (GridView)findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int iconIndex,
+                                    long id) {
+            //Do something when an icon is clicked e.g
+                if (iconIndex == 0){
+                    Toast.makeText(RolesActivity.this, "Icon 0 clicked", Toast.LENGTH_SHORT).show();
+                }
+                else if (iconIndex == 1){
+                    Toast.makeText(RolesActivity.this, "Icon 1 clicked", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
     }
 }
