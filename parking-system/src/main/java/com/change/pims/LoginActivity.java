f077099e62fca829129a61f7e90760e2df9b3bc1
Raymond <rtukpe@gmail.com>
2013-09-11 14:29:13 +0100

    1. re-edited the Login Activity: added the user authentication feature (not completed yet though) 2. created the layout and activity for the Security Activity

--- parking-system/src/main/java/com/change/pims/LoginActivity.java
+++ parking-system/src/main/java/com/change/pims/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.change.pims;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,14 +13,24 @@ import android.widget.EditText;
  * Creating the login activity.
  */
 public class LoginActivity extends Activity {
+
+    private String username;
+    private String password;
+
+    private EditText uName;
+    private EditText pWord;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText uName = (EditText)findViewById(R.id.txtUserName);
-        EditText pWord = (EditText)findViewById(R.id.txtPassword);
-        Button btnSignIn = (Button)findViewById(R.id.btnSignIn);
-        Button btnReg = (Button)findViewById(R.id.btnReg);
+        getLoginAuthentication();
+
+        uName = (EditText)findViewById(R.id.txtUserName);
+        pWord = (EditText)findViewById(R.id.txtPassword);
+
+        final Button btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        final Button btnReg = (Button)findViewById(R.id.btnReg);
 
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,10 +43,39 @@ public class LoginActivity extends Activity {
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), SecurityActivity.class);
-                startActivity(intent);
+                String curUName = uName.getText().toString();
+                String curPWord = pWord.getText().toString();
+
+                if((curUName.equalsIgnoreCase(username) && curPWord.equalsIgnoreCase(password))){
+                    Intent intent = new Intent(view.getContext(), SecurityActivity.class);
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    /**
+     * verifies the current user's authentication
+     */
+    public void getLoginAuthentication(){
+
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);;
+
+        //commented out for testing... the values are already stored in the application
+        /*SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putString("@string/uName", "pims");
+
+        editor.putString("@string/pWord", "00000");
+
+        editor.commit();*/
+
+        /*
+         * in a case where many users exist an array may be used
+         * get all the available values
+         */
+        username = preferences.getString("@string/uName", "null");
+        password = preferences.getString("@string/pWord", "null");
 
     }
 
--- parking-system/src/main/java/com/change/pims/SecurityActivity.java
+++ parking-system/src/main/java/com/change/pims/SecurityActivity.java
@@ -1,13 +1,101 @@
 package com.change.pims;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.change.pims.LoginActivity;
+import com.change.pims.R;
 
 /**
  * implementing the view for the security
  */
 public class SecurityActivity extends Activity {
+
+    private final static int capacity = 50;
+    private int available = 50;
+    private int is_open = 0;
+    private int reserved = 0;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security_view);
+
+        final Button btnIncrease = (Button)findViewById(R.id.btnIncrease);
+        final Button btnDecrease = (Button)findViewById(R.id.btnDecrease);
+        final Button btnClose = (Button)findViewById(R.id.btnClose);
+
+        final TextView txtCapacity = (TextView)findViewById(R.id.txtCapacity);
+        final TextView txtAvailable = (TextView)findViewById(R.id.txtAvailable);
+        final TextView txtIsOpen = (TextView)findViewById(R.id.txtIsOpen);
+        final TextView txtReserved = (TextView)findViewById(R.id.txtReserved);
+
+        txtCapacity.setText(String.valueOf(getCapacity()));
+        txtAvailable.setText(String.valueOf(getAvailable()));
+        txtIsOpen.setText(String.valueOf(getIs_open()));
+        txtReserved.setText(String.valueOf(getReserved()));
+
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(getAvailable() < 50){
+                    setAvailable(getAvailable() + 1);
+                    txtAvailable.setText(String.valueOf(getAvailable()));
+                }
+            }
+        });
+
+        btnDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(getAvailable() > 0){
+                    setAvailable(getAvailable() - 1);
+                    txtAvailable.setText(String.valueOf(getAvailable()));
+                }
+            }
+        });
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), LoginActivity.class);
+
+                //clears the back stack and sets the activity to its root state
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
     }
+
+    public void setReserved(int reserved) {
+        this.reserved = reserved;
+    }
+
+    public void setIs_open(int is_open) {
+        this.is_open = is_open;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public int getIs_open() {
+        return is_open;
+    }
+
+    public int getReserved() {
+        return reserved;
+    }
+
 }
